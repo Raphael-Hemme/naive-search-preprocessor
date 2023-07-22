42939912afce220398676e7e37f5bcaa742fe32b
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-07-22 14:54:45 +0200

    work on reading file content

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-const fs = require('fs');
+import { readFile, readdirSync } from 'fs';
 
-const mdFilePathArr = []
+
+console.log('\n')
+console.log('-- NAIVE SEARCH PREPROCESSOR --')
+console.log('\n')
+console.log('... collecting files')
 
 const generateFilePathArr = (dir) => {
-  const fileNameArr = fs.readdirSync(dir);
+  const fileNameArr = readdirSync(dir);
   return fileNameArr.map(fileName => `${dir}/${fileName}`);
 }
 
-mdFilePathArr.push(generateFilePathArr('./blog-posts'));
+const mdFilePathArr = generateFilePathArr('./blog-posts');
 
-console.log(mdFilePathArr);
+
+console.log('...found the following files: ');
+console.log('\n')
+
+for (const filePath of mdFilePathArr) {
+  console.log(filePath);
+};
+
+
+const getFileContent = (filePath) => {
+
+  let fileContent;
+
+  
+  readFile(filePath, 'utf8', (err, data) => {
+    if (data) {
+      fileContent = data;
+      printFileContent(data);
+    } else {
+      console.error(err);
+    }
+  })
+  
+
+  
+}
+
+const printFileContent = (data) => {
+  console.log('\n');
+  console.log('content of first file: ');
+  console.log('\n');
+
+  console.log(data);
+}
+
+getFileContent(mdFilePathArr[0]);
