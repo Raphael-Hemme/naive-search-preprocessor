cf0a89091d839ac09d5e99f0c39fbb9980460628
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-07-23 21:08:51 +0200

    add funtion to write aggregated searchIndexObj into a json file - not well written with timeouts but working for now

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { readFile, readdirSync } from 'fs';
+import { readFile, readdirSync, writeFile } from 'fs';
 
 
 const matchSymbolsRegEx = /[^a-zA-Z0-9. ]/g;
@@ -16,6 +16,7 @@ const generateFilePathArr = (dir) => {
 }
 
 const mdFilePathArr = generateFilePathArr('./blog-posts');
+const preIndexObjArr = [];
 
 
 console.log('...found the following files: ');
@@ -29,11 +30,11 @@ for (const filePath of mdFilePathArr) {
 const getFileContent = (filePath) => {
   readFile(filePath, 'utf8', (err, data) => {
     if (data) {
-      printFileContent(data, filePath);
+      // printFileContent(data, filePath);
       const cleanedLineSplitArr = generateCleanedLineSplitArr(data);
-      // 
-      const indexObj = generateIndexObj(cleanedLineSplitArr, filePath);
-      console.log(indexObj);
+      // const indexObj = generateIndexObj(cleanedLineSplitArr, filePath);
+      console.log('test ---- pushing now');
+      preIndexObjArr.push(...generatePreIndexObjArr(cleanedLineSplitArr, filePath))
     } else {
       console.error(err);
     }
@@ -47,7 +48,7 @@ const generateCleanedLineSplitArr = (data) => {
     const cleanedLineStr = cleanUpLineStr(l);
     const cleanedLineStrArr = cleanedLineStr.split(' ');
     const cleanedLineStrPartialsArr = cleanedLineStrArr.map(el => generatePartialEntries(el)).flat();
-    console.log(i, cleanedLineStr)
+    // console.log(i, cleanedLineStr)
 
     const result = {
       line: i,
@@ -80,7 +81,7 @@ const cleanUpLineStr = (lineStr) => {
     .trim();
 }
 
-const generateIndexObj = (cleanedLineSplitArr, sourceFilePath) => {
+const generatePreIndexObjArr = (cleanedLineSplitArr, sourceFilePath) => {
   const indexArr = [];
   for (const lineObj of cleanedLineSplitArr) {
     for (const contentE of lineObj.contentArr) {
@@ -98,7 +99,8 @@ const generateIndexObj = (cleanedLineSplitArr, sourceFilePath) => {
     }
   }
 
-  return Object.fromEntries(indexArr);
+  return indexArr;
+  // return Object.fromEntries(indexArr);
 }
 
 const printFileContent = (data) => {
@@ -109,4 +111,36 @@ const printFileContent = (data) => {
   console.log('\n');
 }
 
-getFileContent(mdFilePathArr[0]);
+const writeSearchIndexObjToJsonFile = (searchIndexObj) => {
+  const jsonObj = JSON.stringify(searchIndexObj);
+  writeFile('search-index.json', jsonObj, 'utf8', (err) => {
+    if (err) {
+      console.log('There has been an error: ', err);
+    } else {
+      console.log('Contnet has been written to file.');
+    }
+  })
+}
+
+
+setTimeout(() => {
+  let mdFilePathArrIterator = mdFilePathArr.length;
+  console.log('----------------->', mdFilePathArrIterator)
+
+  for (const path of mdFilePathArr) {
+
+    getFileContent(path);
+    mdFilePathArrIterator -= 1;
+    console.log('iterator: ', mdFilePathArrIterator)
+
+  }
+  
+  setTimeout(() => {
+    if (mdFilePathArrIterator === 0 && preIndexObjArr.length > 0) {
+      writeSearchIndexObjToJsonFile(Object.fromEntries(preIndexObjArr));
+      // console.log('finally got: ', Object.fromEntries(preIndexObjArr));
+    }
+  }, 2000);
+
+}, 100)
+
