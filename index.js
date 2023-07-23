adc6e995f6009497aa2dfac820fe7a4037fe2b25
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-07-23 12:11:55 +0200

    refactor and improve cleanup functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import { readFile, readdirSync } from 'fs';
 
 
+const matchSymbolsRegEx = /[^a-zA-Z0-9. ]/g;
+const matchMultipleSpaceRegEx = /\s{2,}/g;
+
+
 console.log('\n')
 console.log('-- NAIVE SEARCH PREPROCESSOR --')
 console.log('\n')
@@ -23,29 +27,28 @@ for (const filePath of mdFilePathArr) {
 
 
 const getFileContent = (filePath) => {
-  let fileContent;
   readFile(filePath, 'utf8', (err, data) => {
     if (data) {
-      fileContent = data;
       printFileContent(data, filePath);
+      const cleanedLineSplitArr = generateCleanedLineSplitArr(data);
+      const indexObj = generateIndexObj(cleanedLineSplitArr, filePath);
+      console.log(indexObj);
     } else {
       console.error(err);
     }
   }) 
 }
 
-const printFileContent = (data, sourceFilePath) => {
-  console.log('\n');
-  console.log('Content of first file: ');
-  console.log('\n');
-  console.log(data); 
-  console.log('\n');
+const generateCleanedLineSplitArr = (data) => {
 
   let i = 1;
   const newLineSplitArr = data.split('\n').map(l => {
+    const cleanedLineStr = cleanUpLineStr(l)
+    console.log(i, cleanedLineStr)
+
     const result = {
       line: i,
-      contentArr: l.split(' ')
+      contentArr: cleanedLineStr.split(' ')
     }
 
     i += 1;
@@ -53,12 +56,22 @@ const printFileContent = (data, sourceFilePath) => {
   });
 
   console.log(newLineSplitArr);
+  return newLineSplitArr;
+}
 
-  const rawContentArr = [];
-  for (const lineObj of newLineSplitArr) {
+const cleanUpLineStr = (lineStr) => {
+  return lineStr
+    .replaceAll(matchSymbolsRegEx, ' ')
+    .replaceAll(matchMultipleSpaceRegEx, ' ')
+    .trim();
+}
+
+const generateIndexObj = (cleanedLineSplitArr, sourceFilePath) => {
+  const indexArr = [];
+  for (const lineObj of cleanedLineSplitArr) {
     for (const contentE of lineObj.contentArr) {
-      if (contentE && /^(?=.*[A-Za-z0-9]).+$/.test(contentE)) {
-        rawContentArr.push(
+      if (contentE) {
+        indexArr.push(
           {
             [contentE]: {
               file: sourceFilePath,
@@ -69,11 +82,15 @@ const printFileContent = (data, sourceFilePath) => {
       }
     }
   }
+  return Object.fromEntries(indexArr);
+}
 
-  console.log('raw: ', rawContentArr);
-  // const processedArr = newLineSplitArr.map(e => [`${e.cont.toString().toLowerCase()}`, sourceFilePath]);
-
-  // console.log('-> searching for "let": ', stringifiedObj['let'])
+const printFileContent = (data) => {
+  console.log('\n');
+  console.log('Content of first file: ');
+  console.log('\n');
+  console.log(data); 
+  console.log('\n');
 }
 
 getFileContent(mdFilePathArr[0]);
