401d360785d4d67a62fb39eb6024807f93c7adeb
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-08-12 14:12:04 +0200

    work on cli-input to get selected mode and if provided source and / or target paths

--- cli-input.ts
+++ cli-input.ts
@@ -1,5 +1,13 @@
 import process from 'process';
 
+export type Mode = 'CLI' | 'AUTO' | 'HELP' | 'ERROR'; 
+
+export interface SourceAndTargetPathObj {
+    sourcePaths: string[];
+    targetPath: string;
+    errors: string[];
+}
+
 const getRelevantScriptArgs = () => {
     const args = process.argv.slice(2);
     if (args.length < 1) {
@@ -9,4 +17,47 @@ const getRelevantScriptArgs = () => {
     return args;
 }
 
-const getSourcePaths = ()
+const selectMode = (allArgs: string[]): Mode => {
+    if (!allArgs) {
+        return 'CLI';
+    } 
+    
+    if (allArgs.includes('--help')) {
+        return 'HELP';
+    }
+
+    if (allArgs.includes('--source') && allArgs.includes('--target')) {
+        return 'AUTO';
+    } else if (allArgs.includes('--source') || allArgs.includes('--target')) {
+        return 'CLI';
+    } else {
+        return 'ERROR';
+    };
+}
+
+const extractSourceAndTargetPathsFromArgs = (allArgs: string[]): SourceAndTargetPathObj => {
+    const resultObj: SourceAndTargetPathObj = {
+        sourcePaths: [],
+        targetPath: '',
+        errors: []
+    }
+
+    let sourcePathsStartIndicator = allArgs.findIndex(arg => arg === '--source');
+    let targetPathsStartIndicator = allArgs.findIndex(arg => arg === '--target');
+
+    if (sourcePathsStartIndicator !== -1 && targetPathsStartIndicator !== -1) {
+        resultObj.sourcePaths = allArgs.slice(sourcePathsStartIndicator + 1, targetPathsStartIndicator);
+        resultObj.targetPath = allArgs[targetPathsStartIndicator + 1];
+    } else if (sourcePathsStartIndicator !== -1 && targetPathsStartIndicator === -1) {
+        resultObj.sourcePaths = allArgs.slice(sourcePathsStartIndicator + 1);
+        resultObj.errors.push('missing target path');
+    } else if (sourcePathsStartIndicator === -1 && targetPathsStartIndicator !== -1) {
+        resultObj.targetPath = allArgs[targetPathsStartIndicator + 1];
+        resultObj.errors.push('missing source paths');
+    } else {
+        resultObj.errors.push('missing source paths');
+        resultObj.errors.push('missing target path');
+    }
+
+    return resultObj;
+}
