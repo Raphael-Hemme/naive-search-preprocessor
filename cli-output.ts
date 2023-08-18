a7b136a68fd6aaf01ee6ab5229c83c60119da279
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2023-08-18 19:28:12 +0200

    Add printHelp function for the help mode to cli-output

--- cli-output.ts
+++ cli-output.ts
@@ -2,8 +2,6 @@ export const printFrontMatter = (): void => {
     console.log('\n')
     console.log('-- NAIVE SEARCH PREPROCESSOR --')
     console.log('\n')
-    console.log('... collecting files')
-    console.log('\n')
 }
 
 export const printFilePaths = (filePathArr: string[]): void => {
@@ -12,4 +10,18 @@ export const printFilePaths = (filePathArr: string[]): void => {
         console.log('\t', filePath);
     }
     console.log('\n')
+}
+
+export const printHelp = (): void => {
+    console.log('NAIVE SEARCH PREPROCESSOR - HELP\n\n');
+    console.log('To run the program in auto mode, use the following command:');
+    console.log('\tnode index.js --source <source path> --target <target path>');
+    console.log('where <source path> is the path or a space separated list of paths to the directory / directories containing the files you want to index , and <target path> is the path to the file you want to write the index to.\n');
+
+    console.log('To run the program in interactive mode / CLI mode, use the following command:');
+    console.log('\tnode index.js');
+    console.log('You will then be prompted to enter the paths to the source files and the target file.\n');
+
+    console.log('To see this help text, use the following command:');
+    console.log('\tnode index.js --help\n');
 }
